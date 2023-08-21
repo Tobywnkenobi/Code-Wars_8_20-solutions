a350a8dcfcd0d5ec43fab25caba1c44cebaf1985
TobyM <tobymarkham@gmail.com>
2023-08-20 21:41:14 -0400

    make_negative complete, working bool TF

--- convert_bool.py
+++ convert_bool.py
@@ -1,19 +1,4 @@
-# def make_negative( number ):
-#     pass
-# Given a number "numb"
-# Have to make it make_negative
-# Leave negative numbers alone
+#Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false
 
-test_num=42
-
-def make_negative(number):
-    if number >= 0: 
-        neg_number = 0 - number
-        return neg_number
-    else:
-        return number
-
-
- 
-    
-    # pass
+def bool_to_word(boolean):
+    # TODO
---
+++ make_negative.py
@@ -0,0 +1,19 @@
+# def make_negative( number ):
+#     pass
+# Given a number "numb"
+# Have to make it make_negative
+# Leave negative numbers alone
+
+test_num=42
+
+def make_negative(number):
+    if number >= 0: 
+        neg_number = 0 - number
+        return neg_number
+    else:
+        return number
+
+
+ 
+    
+    # pass
